b43548b23c33f47ba6275982b6529d68eaa8e3ff
“cerkesbilal” <bilalkarademir86@gmail.com>
2025-06-30 10:30:33 +0300

    konu anlatimi1

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,73 @@ class _TasarimTekrarState extends State<TasarimTekrar> {
         backgroundColor: Colors.pink,
         foregroundColor: Colors.white,
       ),
-      body: Container(),
+      body: Container(
+        color: Colors.red,
+        child: Text("Merhaba"*34),
+        constraints: BoxConstraints(
+          minHeight: 100,
+          minWidth: 100,
+          maxHeight: 200,
+          maxWidth: 300,
+        ),
+      ),
     );
   }
 }
+
+/*
+Container:
+-Kutu anlamına gelir. 
+-Container eğer çocuğu yoksa parent kadar yer kaplar. Eğer çocuk atamıyorsan mutlaka en boy vermek zorundasın.
+ body: Container(
+        color: Colors.red,
+      ),
+
+      body: Container(
+        height: 200,
+        color: Colors.red,
+      ),
+-Eğer bir çocuk veriyorsanız onun kadar yer kaplar. Ama onun kadar da yer kaplamasını istemiyorsan en boy vermelisin.
+ body: Container(
+        color: Colors.red,
+        child: Text("Merhaba"),
+      ),
+
+-Bir container içindeki çocuğun konumunu belirtebilirsiniz.      
+Container(
+        //alignment: Alignment(1,-1),
+        alignment: Alignment.centerLeft,
+        height: 80,
+        width: 200,
+        color: Colors.red,
+        child: Text("Merhaba"),
+      ),
+
+Eğer bir container a boyut vermediysek aligment özelliği var ise çocuğu olsa da alanı parent kadardır. Yani burada tüm alanı kaplayacak
+
+ body: Container(
+        //alignment: Alignment(1,-1),
+        alignment: Alignment.centerLeft,
+        color: Colors.red,
+        child: Text("Merhaba"),
+    
+      ),
+
+      Bunu istemiyorsak en ya da boy vermeliyiz. Hangisini verirsek vermediğimiz o yönde yayılır. 
+
+-Ben container içindeki veri kadar değil ama sabit en boy kadar alan belirlemek istiyorsam. 
+
+Container(
+        color: Colors.red,
+        child: Text("Merhaba"*34),
+        constraints: BoxConstraints(
+          minHeight: 100,
+          minWidth: 100,
+          maxHeight: 200,
+          maxWidth: 300,
+        ),
+      ),
+
+      
+
+*/
