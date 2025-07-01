0bd029e4856fa7aaaafdd35dc92754f523c209ba
“cerkesbilal” <bilalkarademir86@gmail.com>
2025-07-01 11:51:21 +0300

    ekstra duzeltme

--- lib/expanded_kullanimi.dart
+++ lib/expanded_kullanimi.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 
-class ExpandedKullanimi extends StatefulWidget {
+class ExpandedKullanimi
+    extends StatefulWidget {
   const ExpandedKullanimi({super.key});
 
   @override
-  State<ExpandedKullanimi> createState() => _ExpandedKullanimiState();
+  State<ExpandedKullanimi>
+  createState() =>
+      _ExpandedKullanimiState();
 }
 
-class _ExpandedKullanimiState extends State<ExpandedKullanimi> {
+class _ExpandedKullanimiState
+    extends State<ExpandedKullanimi> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
